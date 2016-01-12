cffa6bace427d899c7f7f71de646850d6f0d1b21
Stefan Pamparau <stefan.pamparau@iquestgroup.com>
2016-01-12 12:45:59 +0200

    Removed unnecessary field from ClientService.

--- service/src/main/java/service/impl/DefaultClientService.java
+++ service/src/main/java/service/impl/DefaultClientService.java
@@ -10,11 +10,9 @@ import service.DatabaseService;
  * Created by stefan.pamparau on 1/12/2016.
  */
 public class DefaultClientService implements ClientService {
-    private DatabaseService databaseService;
     private ClientDAO clientDAO;
 
     public DefaultClientService(DatabaseService databaseService) {
-        this.databaseService = databaseService;
         this.clientDAO = new DefaultClientDAO(databaseService.getConnectionController());
     }
 
